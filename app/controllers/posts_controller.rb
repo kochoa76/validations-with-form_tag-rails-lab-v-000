1c88728e5ee10188b7743e6de3eedfc99f3fb613
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-03 01:54:11 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,11 @@ class PostsController < ApplicationController
   end
 
   def update
-  @post = Post.find(params[:id])
-    if @post.valid?
-      @post.update(post_params)
-      redirect_to post_path(@post)
-    else
-      render :edit
-    end 
+    @post = Post.find(params[:id])
+
+    @post.update(post_params)
+
+    redirect_to post_path(@post)
   end
 
   private
