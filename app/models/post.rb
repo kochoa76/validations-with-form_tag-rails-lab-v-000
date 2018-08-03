7bb2ee19c9e17bdd9373da5a7f98f9cfab9f7ccc
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-03 01:49:56 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {is: 10}
+  validates :category, inclusion: {in: %w(Fiction Non-fiction)}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :authors, only: [:new, :create, :edit, :show]
+  resources :posts, only: [:new, :edit, :update, :show]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
