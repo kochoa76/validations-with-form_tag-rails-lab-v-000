8be70c0c913e4e1e5f221a122bedffdc31f20bb3
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-03 02:10:01 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,15 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  resources :authors, only: [:new, :create, :edit, :show]
-  resources :posts, only: [:new, :edit, :update, :show]
+  Rails.application.routes.draw do
+  get "/authors/new", to: "authors#new", as: "new_author"
+  get "/authors/:id", to: "authors#show", as: "author"
+  post "/authors", to: "authors#create"
+
+  get "/posts/:id", to: "posts#show", as: "post"
+  get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  patch "/posts/:id", to: "posts#update"
+end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
