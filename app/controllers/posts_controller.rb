bc4f258246d4966a5f196b8edaf87390ef3fe7b2
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-03 02:06:08 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,79 @@
 class PostsController < ApplicationController
+  before_action :set_post!, only: [:show, :edit, :update]
+
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-     if @post.valid?
-       @post.update(post_params)
-       redirect_to post_path(@post)
-     else
-       render :edit
-     end
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+
+  def set_post!
+    @post = Post.find(params[:id])
+  end
+endclass PostsController < ApplicationController
+  before_action :set_post!, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :category, :content)
+  end
+
+  def set_post!
+    @post = Post.find(params[:id])
+  end
+endclass PostsController < ApplicationController
+  before_action :set_post!, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :category, :content)
+  end
+
+  def set_post!
+    @post = Post.find(params[:id])
   end
 end
